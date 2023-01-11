c7c04349ea30f12ede6292b7efef4bd074b37853
afterglowss <115690299+afterglowss@users.noreply.github.com>
2023-01-11 19:57:26 +0900

    휴대폰 넣고 빼는 거 완전히 구현!
    휴대폰 넣고 빼는 거 구현 완료. 휴대폰 뺐을 때 버튼을 클릭하지 못하게 한다던가 하는 자잘한 부분은 이후 수정 예정.

--- RemoveUnity/Assets/Script/RightMove.cs
+++ RemoveUnity/Assets/Script/RightMove.cs
@@ -5,26 +5,27 @@ using UnityEngine.UI;
 
 public class RightMove : MonoBehaviour
 {
-    public Image presentImage;
+    private Image presentImage;
     private Camera mainCamera;
 
     private void Awake()
     {
         mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        presentImage = transform.parent.GetComponent<Image>();
     }
     public void OnBtnClick()
     {
         if (presentImage.name == "Room1")
         {
-            mainCamera.transform.position = new Vector3(17.78f,0, -100);
+            mainCamera.transform.position = new Vector3(17.77778f,0, -100);
         }
         else if (presentImage.name == "Room2")
         {
-            mainCamera.transform.position = new Vector3(17.78f, -10, -100);
+            mainCamera.transform.position = new Vector3(35.5556f, 0, -100);
         }
         else if (presentImage.name == "Room3")
         {
-            mainCamera.transform.position = new Vector3(0, -10, -100);
+            mainCamera.transform.position = new Vector3(53.3334f, 0, -100);
         }
         else if (presentImage.name == "Room4")
         {
