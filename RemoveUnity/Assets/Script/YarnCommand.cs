b0d14db86b5b25f18bc5d85a7b5ba0b03e339dea
nahyunKoo <angelcindy09@gmail.com>
2023-02-15 03:45:00 +0900

    얀스크립트/ 이미지 관련 c# 작성 완료
    아직 남은 부분
    1. 비밀번호 입력 구현
    2. UI 부분

--- RemoveUnity/Assets/Script/YarnCommand.cs
+++ RemoveUnity/Assets/Script/YarnCommand.cs
@@ -181,6 +181,293 @@ public class YarnCommand : MonoBehaviour
     {
         GameObject.Find("FadeCanvas").transform.Find("MessageScreencapture").gameObject.SetActive(false);
     }
+
+    //협박문자 확대 사진
+    [YarnCommand("messageZoomIn")]
+    public static void MessageZoomIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("MessageScreencaptureZoom").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("messageZoomOut")]
+    public static void MessageZoomOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("MessageScreencaptureZoom").gameObject.SetActive(false);
+    }
+
+    //진단서 사진
+    [YarnCommand("medicalCertificateIn")]
+    public static void medicalCertificateIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("MedicalCertificate").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("medicalCertificateOut")]
+    public static void medicalCertificateOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("MedicalCertificate").gameObject.SetActive(false);
+    }
+
+    //진단서 확대 사진
+    [YarnCommand("medicalCertificateZoomIn")]
+    public static void medicalCertificateZoomIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("MedicalCertificateZoom").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("medicalCertificateZoomOut")]
+    public static void medicalCertificateZoomOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("MedicalCertificateZoom").gameObject.SetActive(false);
+    }
+
+    //4번 사진
+    [YarnCommand("picture4In")]
+    public static void picture4In()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Picture4").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("picture4Out")]
+    public static void picture4Out()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Picture4").gameObject.SetActive(false);
+    }
+
+    //지연이 한강 대화사진
+    [YarnCommand("hanriverIn")]
+    public static void hanriverIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("HanriverTxt").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("hanriverOut")]
+    public static void hanriverOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("HanriverTxt").gameObject.SetActive(false);
+    }
+
+    //8-2번 사진
+    [YarnCommand("picture8_2In")]
+    public static void picture8_2In()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Picture8_2").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("picture8_2Out")]
+    public static void picture8_2Out()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Picture8_2").gameObject.SetActive(false);
+    }
+
+    //지연이 본가 대화 사진
+    [YarnCommand("mainhouseIn")]
+    public static void mainhouseIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Mainhouse").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("mainhouseOut")]
+    public static void mainhouseOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Mainhouse").gameObject.SetActive(false);
+    }
+
+    //띄어쓰기 사진
+    [YarnCommand("WordspacingIn")]
+    public static void WordspacingIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Wordspacing").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("WordspacingOut")]
+    public static void WordspacingOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Wordspacing").gameObject.SetActive(false);
+    }
+
+    //전공책 사진
+    [YarnCommand("MajorbookIn")]
+    public static void MajorbookIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Majorbook").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("MajorbookOut")]
+    public static void MajorbookOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Majorbook").gameObject.SetActive(false);
+    }
+
+    //사회 심리학 사진
+    [YarnCommand("SocialpsyIn")]
+    public static void SocialpsyIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Socialpsy").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("SocialpsyOut")]
+    public static void SocialpsyOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Socialpsy").gameObject.SetActive(false);
+    }
+
+    //재환 사회 심리학 사진
+    [YarnCommand("SocialpsyTxtIn")]
+    public static void SocialpsyTxtIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("SocialpsyTxt").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("SocialpsyTxtOut")]
+    public static void SocialpsyTxtOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("SocialpsyTxt").gameObject.SetActive(false);
+    }
+
+    //운동화 사진
+    [YarnCommand("SneakerIn")]
+    public static void SneakerIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Sneaker").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("SneakerOut")]
+    public static void SneakerOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Sneaker").gameObject.SetActive(false);
+    }
+
+    //구두 사진
+    [YarnCommand("ShoesIn")]
+    public static void ShoesIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Shoes").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("ShoesOut")]
+    public static void ShoesOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Shoes").gameObject.SetActive(false);
+    }
+
+    //반지 사진
+    [YarnCommand("RingIn")]
+    public static void ringIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Ring").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("RingOut")]
+    public static void ringOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Ring").gameObject.SetActive(false);
+    }
+
+
+    //또다른 반지 사진
+    [YarnCommand("RingAnotherIn")]
+    public static void RingAnotherIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("RingAnother").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("RingAnotherOut")]
+    public static void RingAnotherOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("RingAnother").gameObject.SetActive(false);
+    }
+
+    //오하나 대화내역 사진
+    [YarnCommand("TxtWithHanaIn")]
+    public static void TxtWithHanaIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("TxtWithHana").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("TxtWithHanaOut")]
+    public static void TxtWithHanaOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("TxtWithHana").gameObject.SetActive(false);
+    }
+
+    //캘린더 확대 사진
+    [YarnCommand("CalenderDIn")]
+    public static void CalenderDIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("CalenderD").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("CalenderDOut")]
+    public static void CalenderDOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("CalenderD").gameObject.SetActive(false);
+    }
+
+    //의문의 어플 사진
+    [YarnCommand("ApplicationLIn")]
+    public static void ApplicationLIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("ApplicationL").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("ApplicationLOut")]
+    public static void ApplicationLOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("ApplicationL").gameObject.SetActive(false);
+    }
+
+    //5번 사진
+    [YarnCommand("picture5In")]
+    public static void picture5In()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Picture5").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("picture5Out")]
+    public static void picture5Out()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Picture5").gameObject.SetActive(false);
+    }
+
+    //손가락 반지 사진
+    [YarnCommand("FingerInRingIn")]
+    public static void FingerInRingIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("FingerInRing").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("FingerInRingOut")]
+    public static void FingerInRingOut()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("FingerInRing").gameObject.SetActive(false);
+    }
+
+    //8월 28일 메모 사진
+    [YarnCommand("Memo_8_28In")]
+    public static void Memo_8_28In()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Memo_8_28").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("Memo_8_28Out")]
+    public static void Memo_8_28Out()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("Memo_8_28").gameObject.SetActive(false);
+    }
+
+    //재환 신신당부 사진
+    [YarnCommand("TxtWithJaeIn")]
+    public static void TxtWithJaeIn()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("TxtWithJae").gameObject.SetActive(true);
+    }
+
+    [YarnCommand("TxtWithJaeOut")]
+    public static void TxtWithJae()
+    {
+        GameObject.Find("FadeCanvas").transform.Find("TxtWithJae").gameObject.SetActive(false);
+    }
 }
 
 
