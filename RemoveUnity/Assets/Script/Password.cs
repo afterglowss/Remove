0fe35b3a40f7d2cfe7c007d47186b7236fe60734
nahyunKoo <angelcindy09@gmail.com>
2023-08-18 00:56:52 +0900

    레이어 수정 및 짜잘한 기능 추가

--- RemoveUnity/Assets/Script/Password.cs
+++ RemoveUnity/Assets/Script/Password.cs
@@ -12,25 +12,42 @@ public class Password : MonoBehaviour
     public TMP_InputField inputfield_birthday;
     public Button Enter;
     public GameObject BirthdayImage;
+    TMP_InputField inputfield;
 
     private string password = "0828";
+    
     public void Start()
     {
         Runner = FindObjectOfType<DialogueRunner>();
     }
+
+    private void Update()
+    {
+        if(inputfield_birthday.text.Length > 0 && Input.GetKeyUp(KeyCode.Return))
+        {
+            EnterClick();
+        }
+
+        if(inputfield_birthday.text.Length > 4)
+        {
+            inputfield_birthday.text = "";
+        }
+    }
     public void EnterClick()
     {
-        if(inputfield_birthday.text.Equals(password))
+        if(inputfield_birthday.text.Equals(password) && inputfield_birthday.text.Length>0)
         {
             Debug.Log("성공");
             GameObject.Find("BirthdayImage").SetActive(false);
             Runner.StartDialogue("PasswordCorrect");
         }
 
-        else
+        else if(inputfield_birthday.text.Length>0)
         {
             Debug.Log("실패");
             Runner.StartDialogue("InferingPassword");
+            inputfield_birthday.text = "";
         }
     }
+
 }
